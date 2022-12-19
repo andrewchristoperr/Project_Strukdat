da1f2f2905810b37247077b52fd19f2f24574add
andrewchristoperr <andrewch03.ac@gmail.com>
2022-12-19 18:57:28 +0700

    update menu admin

--- OrderManagementSystem.py
+++ OrderManagementSystem.py
@@ -3,8 +3,9 @@ class Node:
         self.next = None
         self.nama = nama
         self.harga = harga
-        self.quantity = None
+        self.quantity = 0
         self.rating = None
+        self.notes = None
 
 class Menu:
     def __init__(self):
@@ -22,60 +23,238 @@ class Menu:
                 temp = temp.next
             temp.next = newNode
 
+    def hapus(self, key):
+        temp = self.head
+
+        if self.head is not None:
+            if temp.nama == key:
+                self.head = temp.next
+                temp = None
+                return
+
+        while temp is not None:
+            if temp.nama == key:
+                break
+            prev = temp
+            temp = temp.next
+        
+        if temp == None:
+            return
+
+        prev.next = temp.next
+        temp = None
+
     def printList(self):
         temp = self.head
         i = 0
         while temp:
             i+=1
-            print(i ,temp.nama, temp.harga)
+            print(i ,'.', temp.nama, temp.harga)
             temp = temp.next
 
 class Order:
     def __init__(self):
         self.head = None
+        self.next = None
 
     def add(self, nama, harga):
         temp = self.head
+        bool = False
         while temp:
             if nama == temp.nama:
-                self.quantity =+ 1
+                temp.quantity += 1
+                
+                bool = True
+                break
             temp = temp.next
-        if self.head == None:
-            newNode = Node(nama, harga)
-            self.head = newNode
+        if bool == False:
+            if self.head == None:
+                newNode = Node(nama, harga)
+                self.head = newNode
+                newNode.quantity += 1
+            else:
+                newNode = Node(nama, harga)
+                temp = self.head
+                while temp.next:
+                    temp = temp.next
+                temp.next = newNode
+                newNode.quantity += 1
+
+    def hapus(self, key):
+        temp = self.head
+
+        if self.head is not None:
+            if temp.nama == key:
+                self.head = temp.next
+                temp = None
+                return
+
+        while temp is not None:
+            if temp.nama == key:
+                break
+            prev = temp
+            temp = temp.next
+        
+        if temp == None:
+            return
+
+        prev.next = temp.next
+        temp = None
+    
+    def isEmpty(self):
+        if self.head is None:
+            return True
         else:
-            newNode = Node(nama, harga)
-            temp = self.head
-            while temp.next:
-                temp = temp.next
-            temp.next = newNode
+            return False
 
     def printList(self):
         temp = self.head
         i = 0
         while temp:
-            i+=1
-            print(i ,temp.nama)
+            if temp.notes == None:
+                print(temp.quantity, temp.nama, temp.quantity * temp.harga)
+            else:
+                print(temp.quantity, temp.nama, temp.quantity * temp.harga, '            Notes', temp.notes)
             temp = temp.next
+
+    def tambahOrder(self, temp, nomer):
+        i = 0
+        while temp:
+            i += 1
+            if i == nomer:
+                self.add(temp.nama, temp.harga)
+            temp = temp.next
+
+    def hapusOrder(self, nama):
+        nama.upper()
+        bool = False
+        temp = self.head
+        while temp:
+            if temp.nama == nama:
+                self.hapus(nama)
+                bool = True
+                temp = temp.next
+        if bool == False:
+            print("Makanan tidak terdaftar")
+
+    def editOrder(self, nama):
+        quantity = int(input("Masukkan quantity: "))
+        nama.upper()
+        bool = False
+        temp = self.head
+        while temp:
+            if temp.nama == nama:
+                temp.quantity = quantity
+                bool = True
+            temp = temp.next
+        if bool == False:
+            print("Makanan tidak terdaftar")
+    def addNotes(self, nama):
+        notes = input("Notes: ")
+        bool = False
+        temp = self.head
+        while temp:
+            if temp.nama == nama:
+                temp.notes = notes
+                bool = True
+            temp = temp.next
+        if bool == False:
+            print("Makanan tidak terdaftar")
+
+def customer():
+    print("Menu: ")
+    menu.printList()
+    print()
+    choice = 0
+    order = Order()
+    while(choice != 6):
+        print("Keranjang: ")
+        order.printList()
+        print()
+        print("1. Tambah Order")
+        print("2. Delete Order")
+        print("3. Confirm Order")
+        print("4. Edit Order")
+        print("5. Tambah Notes")
+        print("6. Exit")
+        choice = int(input("Choice: "))
+        print()
+        if choice == 1:
+            nomer = int(input("Masukkan nomer order pada menu: "))
+            if nomer == 1:
+                order.add(menu.head.nama, menu.head.harga)
+            elif nomer == 2:
+                temp = menu.head
+                order.tambahOrder(temp, nomer)
+            elif nomer == 3:
+                temp = menu.head
+                order.tambahOrder(temp, nomer)
+        elif choice == 2:
+            if order.isEmpty() is True:
+                print("Keranjang masih kosong")
+                print()
+            else:
+                nama = input("Masukkan nama makanan yang ingin dicancel: ")
+                order.hapusOrder(nama)
+        elif choice == 4:
+            if order.isEmpty() is True:
+                print("Keranjang masih kosong")
+                print()
+            else:
+                nama = input("Masukkan nama makanan yang ingin diedit quantitynya: ")
+                order.editOrder(nama)
+        elif choice == 5:
+            if order.isEmpty() is True:
+                print("Keranjang masih kosong")
+                print()
+            else:
+                nama = input("Masukkan nama makanan yang ingin diberi notes: ")
+                order.addNotes(nama)
 menu = Menu()
-menu.add("Kentang", 20000)
-menu.add("Nasi goreng", 25000)
-menu.add("Air Putih", 3000)
-print("Menu: ")
-menu.printList()
-print()
-choice = 0
-order = Order()
-while(choice != 3):
-    print("1. Tambah Order")
-    print("2. Delete Order")
-    print("3. Confirm Order")
-    print("Your Order:")
-    choice = input("Choice: ")
-    if choice == 1:
-        nomer = input("Masukkan nomer order pada menu: ")
-        if nomer == 1:
-            order.add("Kentang")
-        
-
-
+menu.add("KENTANG", 20000)
+menu.add("NASI GORENG", 25000)
+menu.add("AIR PUTIH", 3000)
+password = 'admin123'
+choice = input("Admin: (Ya/Tidak)\n")
+choice.lower()
+inputPass = ''
+if choice == 'ya':
+    while inputPass != password:
+        inputPass = input("Password: \n")
+        if inputPass == password:
+            pilihan = 0
+            print("------ADMIN------")
+            while(pilihan != 4):
+                print("Menu: ")
+                menu.printList()
+                print()
+                print("1. Tambah Menu")
+                print("2. Delete Menu")
+                print("3. Edit Menu")
+                print("4. Exit")
+                pilihan = int(input("Choice: "))
+                print()
+                if pilihan == 1:
+                    namaMenu = input("Masukkan nama menu: ").upper()
+                    harga = int(input("Masukkan harga menu: "))
+                    menu.add(namaMenu, harga)
+                elif pilihan == 2:
+                    nama = input("Masukkan nama menu yang mau didelete: ").upper()
+                    menu.hapus(nama)
+                elif pilihan == 3:
+                    nama =  input("Masukkan nama menu yang mau diedit: ").upper()
+                    nama2 = input("Masukkan nama menu baru: ").upper()
+                    harga = int(input("Masukkan harga: "))
+                    temp = menu.head
+                    while temp:
+                        if temp.nama == nama:  
+                            temp.nama = nama2
+                            temp.harga = harga
+                        temp = temp.next
+                elif pilihan == 4:
+                    customer()  
+        else:
+            print("PASSWORD SALAH!")
+            print()
+else:
+    customer()
